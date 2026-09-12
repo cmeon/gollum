96d5b2ce2a9f4fc01ac500a4b056bbd1760a0c33
Simeon Mugisha <cmeo226@yahoo.com>
2026-09-12 21:40:50 +0300

    update httpoison dependencies

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule Gollum.Mixfile do
 
   defp deps do
     [
-      {:httpoison, "~> 2.2"},
+      {:httpoison, "~> 3.0.0"},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false}
     ]
   end
